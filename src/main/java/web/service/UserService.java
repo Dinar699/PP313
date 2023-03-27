6d96724b631f330d505c12ed33bee918b46d1c77
Dinar699 <d.siraziev88@gmail.com>
2023-03-27 16:11:48 +0300

    first commit

---
+++ src/main/java/web/service/UserService.java
@@ -0,0 +1,17 @@
+package web.service;
+
+import web.model.User;
+
+import java.util.List;
+
+public interface UserService {
+    List<User> getAllUser();
+
+    void add(User user);
+
+    User getUser(Long id);
+
+    void deleteUser(Long id);
+
+    void updateUser(User user);
+}
